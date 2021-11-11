3ace6643ba5e879ab8d81f6f0d6106e6eb13c530
bijaykumarpun <bijay.workplace@gmail.com>
2021-11-11 07:44:31 +0545

    associate navigation host with a navigation graph

--- app/src/main/java/test/navigation/base/BaseActivity.kt
+++ app/src/main/java/test/navigation/base/BaseActivity.kt
@@ -9,8 +9,8 @@ abstract  class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button1).setOnClickListener{ onClickAction1()}
-        findViewById<Button>(R.id.button2).setOnClickListener{onClickAction2()}
+//        findViewById<Button>(R.id.button1).setOnClickListener{ onClickAction1()}
+//        findViewById<Button>(R.id.button2).setOnClickListener{onClickAction2()}
 
     }
 
